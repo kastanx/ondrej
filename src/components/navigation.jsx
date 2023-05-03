8375c351c7f5cb362a6ac55ebaa0389a9d7a035d
kastanx <42181385+kastanx@users.noreply.github.com>
2023-05-03 21:51:14 +0200

    pica

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            ondrej aneurym
+            ondrej aneurysm
           </a>{" "}
         </div>
 
