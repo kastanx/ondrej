e02acf4b9808b266d557e4047d10ec3c8a43e052
kastanx <42181385+kastanx@users.noreply.github.com>
2023-05-08 11:46:54 +0200

    upravy

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,10 +4,23 @@ export const Header = (props) => {
   return (
     <header id="header">
       <div className="intro">
+        <div className="background-image"></div>
         <div className="overlay">
           <div className="container">
             <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text"></div>
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  freedom is out on 15/5/2023
+                  <span></span>
+                </h1>
+                {/* <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <a
+                  href="#features"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Learn More
+                </a>{" "} */}
+              </div>
             </div>
           </div>
         </div>
