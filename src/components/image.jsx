acbc40cd87954ad702b56206245d7e7a1830e6ec
kastanx <42181385+kastanx@users.noreply.github.com>
2023-05-08 12:42:07 +0200

    seo

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -5,9 +5,7 @@ export const Image = ({ title, largeImage, smallImage }) => {
     <div className="portfolio-item">
       <div className="hover-bg">
         <a href={largeImage} title={title} data-lightbox-gallery="gallery1">
-          <div className="hover-text">
-            <h4>{title}</h4>
-          </div>
+          <div className="hover-text"></div>
           <img src={smallImage} className="img-responsive" alt={title} />
         </a>
       </div>
