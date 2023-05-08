52fe3c2080c0e53a3b7d078a57dd39f7932c11dc
kastanx <42181385+kastanx@users.noreply.github.com>
2023-05-08 13:03:01 +0200

    o me

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,8 +10,16 @@ export const About = (props) => {
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <h2>O mně</h2>
+              <p>
+                Jsem hudebník z Ostravy, který ve 14 letech se začal jako samouk
+                hrát na kytaru a skládat své vlastní písně. Největší impuls k
+                tomu bylo, když jsem poznal Nirvanu a vůbec kult Kurta Cobaina.
+                Do dneška je to v mé muzice zřejmě slyšet a asi i vždycky bude.
+                Písně skládám o svém životě a nebo životech, které jsem viděl a
+                poznal. Chci, aby ti, kteří se cítí podobně a zažívají stejné
+                věci věděli, že nejsou sami. Peace, Love & Empathy
+              </p>
             </div>
           </div>
         </div>
